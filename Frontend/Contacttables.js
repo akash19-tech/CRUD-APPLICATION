f8273d4a9749903ce23f34368babd3c60000254d
Akash mishra <97521299+akash19-tech@users.noreply.github.com>
2024-11-17 13:46:19 +0530

    Add files via upload

---
+++ Frontend/Contacttables.js
@@ -0,0 +1,85 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TablePagination,
+  IconButton,
+  Paper,
+} from "@mui/material";
+import { Edit, Delete } from "@mui/icons-material";
+
+const ContactsTable = ({ contacts, editContact, deleteContact }) => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ marginTop: 4 }}>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Phone Number</TableCell>
+              <TableCell>Company</TableCell>
+              <TableCell>Job Title</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {contacts
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((contact, index) => (
+                <TableRow key={index}>
+                  <TableCell>{contact.firstName}</TableCell>
+                  <TableCell>{contact.lastName}</TableCell>
+                  <TableCell>{contact.email}</TableCell>
+                  <TableCell>{contact.phoneNumber}</TableCell>
+                  <TableCell>{contact.company}</TableCell>
+                  <TableCell>{contact.jobTitle}</TableCell>
+                  <TableCell>
+                    <IconButton
+                      onClick={() => editContact(index)}
+                      color="primary"
+                    >
+                      <Edit />
+                    </IconButton>
+                    <IconButton
+                      onClick={() => deleteContact(index)}
+                      color="error"
+                    >
+                      <Delete />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        component="div"
+        count={contacts.length}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+};
+
+export default ContactsTable;
